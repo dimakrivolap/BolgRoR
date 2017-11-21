f1f534de1acbc8a17f76f56fc75ed419fa7eb239
dimakrivolap <krivolap.dima@gmail.com>
2017-11-22 00:24:57 +0300

    add edit ,delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
-
   end
 
   def new
@@ -21,9 +20,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
   private
 
   def post_params
     params.require(:post).permit(:title, :summary , :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+
   end
 end
